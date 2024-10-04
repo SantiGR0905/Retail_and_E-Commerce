7c129bfadc30fb2699d61621aea0e713fd00bb26
DIEGO ALEXANDER PINZON CAMARGO <juliansgutierrez@ucundinamarca.edu.co>
2024-10-04 14:26:00 -0500

    Add Repository, Service and Controller for PermissionsXUsers, Products and Sales

--- Retail/Program.cs
+++ Retail/Program.cs
@@ -15,6 +15,12 @@ builder.Services.AddScoped<IInventoriesRepository, InventoriesRepository>();
 builder.Services.AddScoped<IInventoriesService, InventoriesService>();
 builder.Services.AddScoped<IPermissionsRepository, PermissionsRepository>();
 builder.Services.AddScoped<IPermissionsService, PermissionsService>();
+builder.Services.AddScoped<IPermissionsXUsersRepository, PermissionsXUsersRepository>();
+builder.Services.AddScoped<IPermissionsXUsersService, PermissionsXUsersService>();
+builder.Services.AddScoped<IProductsRepository, ProductsRepository>();
+builder.Services.AddScoped<IProductsService, ProductsService>();
+builder.Services.AddScoped<ISalesRepository, SalesRepository>();
+builder.Services.AddScoped<ISalesService, SalesService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ Retail/Repositories/PermissionsXUsersRepository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Retail.Context;
+using Retail.Model;
+
+namespace Retail.Repositories
+{
+    public interface IPermissionsXUsersRepository
+    {
+        Task<IEnumerable<PermissionsXUsers>> GetPermissionsXUsers();
+        Task<PermissionsXUsers> GetPermissionsXUsersById(int idpermissionxuser);
+        Task CreatePermissionsXUsers(PermissionsXUsers permissionxuser);
+        Task UpdatePermissionsXUsers(PermissionsXUsers permissionxuser);
+        Task SoftDeletePermissionsXUsers(int idpermissionxuser);
+    }
+    public class PermissionsXUsersRepository : IPermissionsXUsersRepository
+    {
+        private readonly RetailDbContext _dbContext;
+
+        public PermissionsXUsersRepository(RetailDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<IEnumerable<PermissionsXUsers>> GetPermissionsXUsers()
+        {
+            return await _dbContext.PermissionsXUsers
+                .Where(s => !s.IsDeleted)
+                .ToListAsync();
+        }
+
+        public async Task<PermissionsXUsers> GetPermissionsXUsersById(int idpermissionxuser)
+        {
+            return await _dbContext.PermissionsXUsers
+                .FirstOrDefaultAsync(s => s.PermissionXUserId == idpermissionxuser && !s.IsDeleted);
+        }
+        public async Task SoftDeletePermissionsXUsers(int idpermissionxuser)
+        {
+            var permissionxuser = await _dbContext.PermissionsXUsers.FindAsync(idpermissionxuser);
+            if (permissionxuser != null)
+            {
+                permissionxuser.IsDeleted = true;
+                await _dbContext.SaveChangesAsync();
+            }
+        }
+
+        public async Task CreatePermissionsXUsers(PermissionsXUsers permissionxuser)
+        {
+            _dbContext.PermissionsXUsers.Add(permissionxuser);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdatePermissionsXUsers(PermissionsXUsers permissionxuser)
+        {
+            _dbContext.PermissionsXUsers.Update(permissionxuser);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
